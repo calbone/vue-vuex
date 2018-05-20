96943e3ad76fdc4c721c7cb1e12a6b0d465d8e54
yu-ku-air <y.kuma1229@gmail.com>
2018-05-20 09:59:57 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
   <div id="app">
-    <Nav-Link></Nav-Link>
-    <router-view></router-view>
+    <img src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import NavLink from './components/NavLink';
-import './styles/app.scss';
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-      NavLink
+    HelloWorld
   }
 }
 </script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,22 +1,8 @@
-import Vue from 'vue';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import locale from 'element-ui/lib/locale/lang/ja';
-import App from './App.vue';
-import store from './store';
-import VueRouter from 'vue-router';
-import routes from './routes';
+import Vue from 'vue'
+import App from './App.vue'
 
-Vue.use(VueRouter);
-Vue.use(ElementUI, {locale});
-
-
-Vue.config.productionTip = false;
-
-const router = new VueRouter(routes);
+Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-  store,
-  router,
-}).$mount('#app');
+  render: h => h(App)
+}).$mount('#app')
