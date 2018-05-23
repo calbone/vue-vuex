c1df96184659c2a3180a3bf86dca8ed139d87335
yu-ku-air <y.kuma1229@gmail.com>
2018-05-24 00:17:42 +0900

    modify routes.js, add login vue directory

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
-import Spam from './components/admin/spam';
+import Signin from '@/components/login/Signin';
+// import Signup from '@/components/login/signup';
+import Spam from '@/components/admin/spam';
 
 export default {
   mode: 'history',
   routes: [
     {
-      path: '/', component: Spam, name: 'spam'
-    }
+      path: '/', component: Spam, name: 'Spam'
+    },
+    {
+      path: '/signin', component: Signin, name: 'Signin'
+    },
   ]
 }
