fc65883378a3f561b86e3440595a260fcca86f63
yu-ku-air <y.kuma1229@gmail.com>
2018-08-11 10:59:30 +0900

    delete firebaseConfig.js

--- firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyApK5OHDrobbtoDmXJ6eRi5MKdasmkC2Vo',
-  authDomain: 'admin-vue-618c7.firebaseapp.com',
-  databaseURL: 'https://admin-vue-618c7.firebaseio.com',
-  projectId: 'admin-vue-618c7',
-  storageBucket: '',
-  messagingSenderId: ''
-}
